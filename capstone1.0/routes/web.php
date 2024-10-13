431ab789cfb548e3854df0fda0b92e2dd9aabe02
AaronSMalicdem <malicdema68@gmail.com>
2024-10-13 08:57:43 +0800

    Admin-UserCRUD

--- capstone1.0/routes/web.php
+++ capstone1.0/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Admin\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,5 +46,15 @@ Route::middleware(['auth'])->group(function () {
         return view('finance.dashboard');
     })->middleware('role:finance_officer');
 
+});
+
+Route::prefix('admin')->middleware(['auth'])->group(function () {
     
+    Route::get('/users', [UserController::class, 'index'])->name('admin.users.index')->middleware('role:admin');
+    Route::get('/users/create', [UserController::class, 'create'])->name('admin.users.create')->middleware('role:admin');
+    Route::post('/users', [UserController::class, 'store'])->name('admin.users.store')->middleware('role:admin');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit')->middleware('role:admin');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('admin.users.update')->middleware('role:admin');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy')->middleware('role:admin');
+
 });
